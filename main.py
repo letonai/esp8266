7f03978834031a4bdbd1bf396a6b50ee3aaf0f67
Ricardo Letonai <letonai@letonai.lan>
2022-04-07 10:41:49 -0700

    .

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 import sys
 
 REMOTE_REPO="https://raw.githubusercontent.com/letonai/esp8266/main/"
-LOCAL_VERSION="0.08"
+LOCAL_VERSION="0.09"
 
 def updateVersion():
   print("Starting update...")
